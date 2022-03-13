f746c592e2a5e625bb9f088319c3c96640d59d10
hodletoshi <hodlegill@protonmail.com>
2022-03-13 12:37:50 -0400

    change details page title

--- frontend/static/js/views/details.js
+++ frontend/static/js/views/details.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
-        this.setTitle("How To Play");
+        this.setTitle("Mint Details");
     }
 
     async getHtml() {
