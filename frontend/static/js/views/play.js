3fe992899fe8706072633e1417871b3e4a8e680d
hodletoshi <hodlegill@protonmail.com>
2022-03-13 22:56:26 -0400

    test hardcode word 1

--- frontend/static/js/views/play.js
+++ frontend/static/js/views/play.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 let guessedWords = [[]];
 let availableSpace = 1;
 
-let word = "wagmi";
+let word = "bubba";
 let guessedWordCount = 0;
 
 var keys = null;
