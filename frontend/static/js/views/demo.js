f5ce44b6088e4c3f2cbf9e1bc5e1658ca2efbba4
hodletoshi <hodlegill@protonmail.com>
2022-04-17 23:59:39 -0400

    change word

--- frontend/static/js/views/demo.js
+++ frontend/static/js/views/demo.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 let guessedWords = [[]];
 let availableSpace = 1;
 
-let word = "alpha";
+let word = "mints";
 let guessedWordCount = 0;
 
 var keys = null;
