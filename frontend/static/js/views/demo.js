c2f9dca2ebdfb48d238ca4705741c135ce2244bb
hodletoshi <hodlegill@protonmail.com>
2022-04-19 00:00:06 -0400

    update word

--- frontend/static/js/views/demo.js
+++ frontend/static/js/views/demo.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 let guessedWords = [[]];
 let availableSpace = 1;
 
-let word = "mints";
+let word = "rares";
 let guessedWordCount = 0;
 
 var keys = null;
